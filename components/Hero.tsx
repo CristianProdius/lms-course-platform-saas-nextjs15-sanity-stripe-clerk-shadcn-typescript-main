e4625ac5ad9b9247ede40dbbf346c08e9fbd00cd
Cristian Prodius <114479945+CristianProdius@users.noreply.github.com>
2025-08-06 23:22:27 +0300

    sdgfhdhj

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import Link from "next/link";
+
 import { Button } from "./ui/button";
 import { ArrowRight, Sparkles } from "lucide-react";
 
